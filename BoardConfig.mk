903491106f69301b5b4ed810d896254768daa54f
RooGhz720 <rooghz720@gmail.com>
2023-03-06 13:19:41 +0800

    redwood: use generic cpu
    Signed-off-by: RooGhz720 <rooghz720@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# default device path
 DEVICE_PATH := device/xiaomi/redwood
 
 # A/B device 
@@ -21,13 +22,15 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := kryo670
-##########
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo670
+
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a78
+TARGET_2ND_CPU_VARIANT := $(TARGET_CPU_VARIANT)
+TARGET_2ND_CPU_VARIANT_RUNTIME := $(TARGET_CPU_VARIANT_RUNTIME)
 
 # SDK
 BOARD_SYSTEMSDK_VERSIONS := 31
